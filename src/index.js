08adf7c8e880a0532e8cd6794bb7b3ca79866291
Alexander Tkachenko <etokimanowka33@rambler.ru>
2020-10-12 20:31:48 +0300

    brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+
+let count=0;
+let open=['(', '{','['];
+let close=[')','}',']'];
+
+for (i=0;i<str.length;i++) {
+  if (open.includes(str[i]))  count++;
+  if (close.includes(str[i])) count--;
+  if (count==-1) { break;}
 }
+
+if (count==0) {
+  return true;
+} else {return false;}
+
+  
+  
+  
+};
